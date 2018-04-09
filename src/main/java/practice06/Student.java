00100493b4319c7b6b33d2613ce75a5c759e20f6
likly <569553290@qq.com>
2018-04-09 21:08:08 +0800

    Practice06Test done

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,29 @@
 package practice06;
 
-public class Student {
+import practice05.Person;
+
+public class Student extends Person {
+
+
+    private Integer klass;
+
+    public Student(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + String.format(" I am a Student. I am at Class %d.", klass);
+    }
 }
+
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,34 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(klass == null) {
+            return super.introduce() + String.format(" I am a Student. I am at No Class.");
+        }else {
+            return super.introduce() + String.format(" I am a Student. I am at Class %d.", klass);
+
+        }
+    }
 }
