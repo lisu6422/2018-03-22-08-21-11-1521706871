98d94c1af5bc828b23d26835de0155db960243fb
likly <569553290@qq.com>
2018-04-09 21:25:49 +0800

    Practice07Test done

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,23 @@
 package practice07;
 
 public class Klass {
+
+    private Integer number;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return number == null ? "No Class" : String.format("Class %d",number);
+    }
+
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,36 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        this(name, age,new Klass(null));
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + String.format(" I am a Teacher. I teach %s.",klass.getDisplayName());
+    }
+
+    public String introduceWith(Student student) {
+
+        if(klass != null && klass.equals(student.getKlass())) {
+            return super.introduce() + String.format(" I am a Teacher. I teach %s.",student.getName());
+        }else {
+            return super.introduce() + String.format(" I am a Teacher. I don't teach %s.",student.getName());
+        }
+    }
 }
