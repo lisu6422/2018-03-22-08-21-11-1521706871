3c92fd804c510b3f0f9cd9a0111dbe3f2d85cc65
likly <569553290@qq.com>
2018-04-09 21:42:09 +0800

    Practice08Test done

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,32 @@
 package practice08;
 
 public class Klass {
+
+    private Integer number;
+    private Student leader;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+
+    public String getDisplayName() {
+        return number == null ? "No Class" : String.format("Class %d",number);
+    }
+
+    public void assignLeader(Student leader) {
+        this.leader = leader;
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,27 @@
 package practice08;
+public class Student extends Person {
+    private Klass klass;
 
-public class Student {
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+
+        if(klass.getLeader() != null && klass.getLeader().getId() == getId()){
+            return super.introduce() + String.format(" I am a Student. I am Leader of %s.",klass.getDisplayName());
+        }
+
+        return super.introduce() + String.format(" I am a Student. I am at %s.",klass.getDisplayName());
+    }
 }
